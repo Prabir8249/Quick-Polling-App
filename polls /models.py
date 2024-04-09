cdd888887d8da7778d24ba686c33955d72429e68
Prabir8249 <88239783+Prabir8249@users.noreply.github.com>
2024-04-09 12:01:31 +0530

    Update models.py

--- polls /models.py
+++ polls /models.py
@@ -1,13 +1,1 @@
-# polls/models.py
-from django.db import models
-from django.contrib.auth.models import User
 
-class Poll(models.Model):
-    question = models.CharField(max_length=200)
-    deadline = models.DateTimeField(null=True, blank=True)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
-
-class Choice(models.Model):
-    poll = models.ForeignKey(Poll, on_delete=models.CASCADE, related_name='choices')
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
